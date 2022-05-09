237e560235562e8d6e83d5b85d57476b27c8cba7
OlegVegan <krutopridumal@gmail.com>
2022-05-09 11:07:20 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// React
+import { useState, useEffect } from "react"
 
-function App() {
+// Components
+import Profile from './components/profile/Profile'
+import UserList from './components/lists/UserList'
+
+// Style
+import './App.scss'
+
+export default function App() {
+  const [selectedUser, setSelectedUser] = useState(null)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-container">
+      {selectedUser && <Profile />}
+      {!selectedUser && <UserList />}
     </div>
-  );
+  )
 }
-
-export default App;
---
+++ src/components/lists/UserList.tsx
@@ -0,0 +1,8 @@
+// Axios
+import Axios from "axios"
+
+export default function UserList() {
+    return (
+        <div>UserList</div>
+    )
+}
